ff8d9d563382124754a0510a77c05ca2e8129c76
i509VCB <git@i509.me>
2020-04-10 19:56:36 -0500

    Make Accessors their own source set

---
+++ essentials-accessors-impl/src/main/java/io/github/nyliummc/essentials/accessor/ExampleMinecraftServerAccessor_PleaseRemoveWhenAnAccessorIsAdded.java
@@ -0,0 +1,8 @@
+package io.github.nyliummc.essentials.accessor;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MinecraftServer.class)
+public interface ExampleMinecraftServerAccessor_PleaseRemoveWhenAnAccessorIsAdded {
+}
