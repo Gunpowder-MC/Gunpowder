58eff4384e05da3833d89187ba5a9b5002b94796
Martmists <martmists@gmail.com>
2020-04-27 17:55:43 +0200

    License headers

--- essentials-module-dynmap/src/main/java/io/github/nyliummc/essentials/mixin/dynmap/DynmapCoreMixin.java
+++ essentials-module-dynmap/src/main/java/io/github/nyliummc/essentials/mixin/dynmap/DynmapCoreMixin.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) NyliumMC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package io.github.nyliummc.essentials.mixin.dynmap;
 
 import io.github.nyliummc.essentials.api.EssentialsMod;
@@ -10,15 +34,19 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.io.*;
 
-@Mixin(DynmapCore.class)
+@Mixin(value={DynmapCore.class}, remap=false)
 class DynmapCoreMixin {
-    @Redirect(method="initConfiguration", at=@At(value="NEW", target="java.io.File", ordinal=0))
+    @Redirect(method="initConfiguration", at=@At(value="NEW", target="Ljava/io/File;"))
     File configFile() throws IOException {
+        System.out.println("We got called!");
+
         File f = new File(FabricLoader.getInstance().getConfigDirectory().getCanonicalPath() + "/essentials-dynmap.yaml");
         if (!f.exists()) {
+            f.createNewFile();
             InputStream in = this.getClass().getClassLoader().getResourceAsStream("essentials-dynmap.yaml");
             OutputStream out = new FileOutputStream(f);
             IOUtils.copy(in, out);
+            System.out.println(f);
         }
         return f;
     }
