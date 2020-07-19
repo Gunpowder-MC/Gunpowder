eb24f9138ace92d2af800b953e87e1a6d117cc34
Martmists <martmists@gmail.com>
2020-07-20 01:27:10 +0200

    Add feedback and currency support, closes #40

--- essentials-module-simpleclaims/src/main/kotlin/io/github/nyliummc/essentials/commands/ClaimCommand.kt
+++ essentials-module-simpleclaims/src/main/kotlin/io/github/nyliummc/essentials/commands/ClaimCommand.kt
@@ -31,10 +31,12 @@ import io.github.nyliummc.essentials.api.builders.Command
 import io.github.nyliummc.essentials.api.module.claims.dataholders.StoredClaim
 import io.github.nyliummc.essentials.api.module.claims.dataholders.StoredClaimAuthorized
 import io.github.nyliummc.essentials.api.module.claims.modelhandlers.ClaimHandler
+import io.github.nyliummc.essentials.api.module.currency.modelhandlers.BalanceHandler
 import io.github.nyliummc.essentials.configs.SimpleClaimsConfig
 import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.command.arguments.GameProfileArgumentType
 import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.text.LiteralText
 import net.minecraft.util.math.ChunkPos
 
 object ClaimCommand {
@@ -44,6 +46,10 @@ object ClaimCommand {
 
     private val handler by lazy {
         EssentialsMod.instance.registry.getModelHandler(ClaimHandler::class.java)
+    }
+
+    private val balanceHandler by lazy {
+        EssentialsMod.instance.registry.getModelHandler(BalanceHandler::class.java)
     }
 
     private val currencyPresent by lazy {
@@ -95,14 +101,16 @@ object ClaimCommand {
 
         if (handler.isChunkClaimed(chunk, context.source.world.registryKey)) {
             // Error, already claimed
+            context.source.sendFeedback(LiteralText("Chunk is already claimed!"), false)
             return 0
         }
 
-        if (currencyPresent && config.price != 0.00) {
+        return if (currencyPresent && config.price != 0.00) {
             // Ask confirm
-            return 1
+            context.source.sendFeedback(LiteralText("Claiming costs $${config.price} please confirm claiming this chunk with /claim confirm"), false)
+            1
         } else {
-            return confirmClaimChunk(context)
+            confirmClaimChunk(context)
         }
     }
 
@@ -110,13 +118,23 @@ object ClaimCommand {
         val chunk = getChunk(context)
 
         if (handler.isChunkClaimed(chunk, context.source.world.registryKey)) {
-            // Error, claimed before us
+            context.source.sendFeedback(LiteralText("Chunk is already claimed!"), false)
             return 0
         }
 
         if (handler.getClaims(context.source.player.uuid).size >= config.maxClaimChunks) {
-            // Error, max claims
+            context.source.sendFeedback(LiteralText("You've already claimed the maximum amount of chunks!"), false)
             return 0
+        }
+
+        if (currencyPresent && config.price != 0.00) {
+            val account = balanceHandler.getUser(context.source.player.uuid)
+            if (account.balance.toDouble() < config.price) {
+                context.source.sendFeedback(LiteralText("Not enough money! Claiming a chunk costs ${config.price}"), false)
+                return 0
+            }
+            account.balance -= config.price.toBigDecimal()
+            balanceHandler.updateUser(account)
         }
 
         handler.createClaim(StoredClaim(
@@ -124,7 +142,7 @@ object ClaimCommand {
                 chunk,
                 context.source.world.registryKey
         ))
-        // Success
+        context.source.sendFeedback(LiteralText("You are now the owner of this chunk"), false)
         return 1
     }
 
@@ -132,13 +150,13 @@ object ClaimCommand {
         val chunk = getChunk(context)
 
         if (!handler.isChunkClaimed(chunk, context.source.world.registryKey)) {
-            // Error, no claim
+            context.source.sendFeedback(LiteralText("This chunk is unclaimed"), false)
             return 0
         }
 
         val owner = EssentialsMod.instance.server.userCache.getByUuid(handler.getClaim(chunk, context.source.world.registryKey).owner)
 
-        // Message
+        context.source.sendFeedback(LiteralText("Chunk owner: ${owner!!.name}"), false)
         return 1
     }
 
@@ -147,23 +165,23 @@ object ClaimCommand {
         val player = GameProfileArgumentType.getProfileArgument(context, "player").first()
 
         if (!handler.isChunkClaimed(chunk, context.source.world.registryKey)) {
-            // Error, no claim
+            context.source.sendFeedback(LiteralText("This chunk is unclaimed"), false)
             return 0
         }
 
         val cfg = handler.getClaim(chunk, context.source.world.registryKey)
         if (cfg.owner != context.source.player.uuid) {
-            // Error, not owner
+            context.source.sendFeedback(LiteralText("You are not the owner of this chunk"), false)
             return 0
         }
 
         if (handler.getClaimAllowed(chunk, context.source.world.registryKey).any { it.user == player.id }) {
-            // Error, already allowed
+            context.source.sendFeedback(LiteralText("This person already has permission to use this chunk"), false)
             return 0
         }
 
         handler.addClaimAllowed(StoredClaimAuthorized(cfg, player.id))
-        // Message
+        context.source.sendFeedback(LiteralText("${player.name} is now permitted to use this chunk"), false)
         return 1
     }
 
@@ -172,43 +190,43 @@ object ClaimCommand {
         val player = GameProfileArgumentType.getProfileArgument(context, "player").first()
 
         if (!handler.isChunkClaimed(chunk, context.source.world.registryKey)) {
-            // Error, no claim
+            context.source.sendFeedback(LiteralText("This chunk is unclaimed"), false)
             return 0
         }
 
         val cfg = handler.getClaim(chunk, context.source.world.registryKey)
         if (cfg.owner != context.source.player.uuid) {
-            // Error, not owner
+            context.source.sendFeedback(LiteralText("You are not the owner of this chunk"), false)
             return 0
         }
 
         if (handler.getClaimAllowed(chunk, context.source.world.registryKey).none { it.user == player.id }) {
-            // Error, already not allowed
+            context.source.sendFeedback(LiteralText("This person already has no permission to use this chunk"), false)
             return 0
         }
 
         handler.removeClaimAllowed(handler.getClaimAllowed(chunk, context.source.world.registryKey).first { it.user == player.id })
-        // Message
+        context.source.sendFeedback(LiteralText("${player.name} is no longer permitted to use this chunk"), false)
         return 1
     }
 
     private fun unclaimChunk(context: CommandContext<ServerCommandSource>): Int {
         val chunk = getChunk(context)
         if (!handler.isChunkClaimed(chunk, context.source.world.registryKey)) {
-            // Error, no claim
+            context.source.sendFeedback(LiteralText("This chunk is unclaimed"), false)
             return 0
         }
 
         val cfg = handler.getClaim(chunk, context.source.world.registryKey)
         if (cfg.owner != context.source.player.uuid) {
-            // Error, not owner
+            context.source.sendFeedback(LiteralText("You are not the owner of this chunk"), false)
             return 0
         }
 
         handler.getClaimAllowed(chunk, context.source.world.registryKey).map(handler::removeClaimAllowed)
         handler.deleteClaim(chunk, context.source.world.registryKey)
 
-        // Message
+        context.source.sendFeedback(LiteralText("This chunk is now available to anyone"), false)
         return 1
     }
 
