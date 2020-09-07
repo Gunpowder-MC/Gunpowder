01525c0fd6667ebe7da9e857640aba3425c8fb32
CODY_AI <ilialarki@gmail.com>
2020-09-07 22:25:53 +0430

    Server brand/mod name now is "Fabric/Gunpowder"

---
+++ gunpowder-base/src/main/java/io/github/gunpowder/mixin/base/MinecraftServerMixin_Base.java
@@ -0,0 +1,15 @@
+package io.github.gunpowder.mixin.base;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin_Base {
+    @Inject(method = "getServerModName", at = @At("HEAD"))
+    private void getGunpowderServerModName(CallbackInfoReturnable<String> cir) {
+        cir.setReturnValue("Fabric/Gunpowder");
+    }
+}
