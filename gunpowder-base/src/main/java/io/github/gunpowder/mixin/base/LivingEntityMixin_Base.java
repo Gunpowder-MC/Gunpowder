5d71c5a14b9e49f1875c00f10e27cdf3c30b7db4
Martmists <martmists@gmail.com>
2021-05-03 21:17:46 +0200

    Small cleanup

--- gunpowder-base/src/main/java/io/github/gunpowder/mixin/base/LivingEntityMixin_Base.java
+++ gunpowder-base/src/main/java/io/github/gunpowder/mixin/base/LivingEntityMixin_Base.java
@@ -38,11 +38,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import static org.checkerframework.checker.units.UnitsTools.g;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin_Base extends Entity {
